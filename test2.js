0cc2ad28f54d2ee656f39ac20debe7788a6e6cd5
Winge Development [Little_Winge] <WingeDevelopment@gmail.com>
2021-12-13 00:03:27 -0500

    Delete test2.js

--- test2.js
+++
@@ -1,7 +0,0 @@
-let string = 'THIS IS A TEST';
-string = string.padEnd(500, 'ţ̵̘̩̪̞̦͇̘͉̘͕͙̹̂̈̎̔͂̎̀̏͋͜͝͝')
-console.log(string);
-console.log(string.length);
-string = string.slice(0, 300);
-console.log(string);
-console.log(string.length);
